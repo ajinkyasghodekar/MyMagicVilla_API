958cfeb6b0136e08470ee046dc52ce1a34972b32
ajinkyasghodekar <ajinkyasghodekar@gmail.com>
2023-04-26 10:46:49 +0530

    Added a HttpPost logic in code

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -5,23 +5,71 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    // [Route("api/controller")]  --> another wahy to route but below one is (line 8) more preffred.
+    // [Route("api/controller")]  --> another way to route but below one is (line 8) more preffred.
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.VillaList;
+            return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("Id:int")]
-        public VillaDTO GetVillas(int id)
+        [HttpGet("Id:int", Name = "GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        // or this can be written as,
+        // [ProducesResponseType(200, Type = typeof(VillaDTO))]
+        // [ProducesResponseType(400)]
+        // [ProducesResponseType(404)]
+        public ActionResult GetVillas(int id)
         {
-            return VillaStore.VillaList.FirstOrDefault(u=>u.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+        {
+            // the below commented code for ModelState is used for validations in place of [ApiController].
+            // But if both are active more prority will be given to [ApiController].
+
+            /*if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }*/
+
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("", "Villa Already Exists");
+                return BadRequest(ModelState);
+            }
+
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id+1;
+            VillaStore.villaList.Add(villaDTO);
+            return CreatedAtRoute("GetVilla", new {id = villaDTO.Id}, villaDTO);
         }
     }
-
 }
-
--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -4,7 +4,7 @@ namespace MagicVilla_VillaAPI.Data
 {
     public static class VillaStore
     {
-        public static List<VillaDTO> VillaList = new List<VillaDTO>
+        public static List<VillaDTO> villaList = new List<VillaDTO>
             {
                 new VillaDTO { Id = 1, Name ="Pool View"},
                 new VillaDTO { Id = 2, Name ="Beach View"}
